79e49f7a82dd5951fe0ea0cf29dabe6276bfaa74
phddone <superdriver405@gmail.com>
2016-04-11 22:38:50 -0700

    p4:add status log

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -11,42 +11,46 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
-        
+        self.status_log = {} #state string, action_index, reward
+        self.q_table = {} # state string -> q_value list
+        # valid_actions = [None, 'forward', 'left', 'right']
+        self.actions = env.valid_actions
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
+
+    def next_action_index(self, current_state):
+        if current_state not in self.q_table.keys():
+            q_table[current_state]= [0, 0, 0, 0]
+        max_action_index = self.q_table[current_state].index(max(q_table[current_state]))
+        
+        # TODO: if has several max, return a random one
+        return max_action_index
 
     def update(self, t):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
-
+        
+        # input = {'light': light, 'oncoming': oncoming, 'left': left, 'right': right}
         # TODO: Update state
+        current_state = (inputs.values() + [self.next_waypoint]).join('#')
+        self.status_log[t] =  tuple(current_state, action, reward)
         
-        # TODO: Select action according to your policy
-        reward = 0
-        
-        if self.next_waypoint == 'right':
-            if inputs['light'] == 'red' and inputs['left'] == 'forward':
-                reward -= 10
-        elif self.next_waypoint == 'forward':
-            if inputs['light'] == 'red':
-                reward -= 10
-        elif self.next_waypoint == 'left':
-            if inputs['light'] == 'red' or (inputs['oncoming'] == 'forward' or inputs['oncoming'] == 'right'):
-                reward -=10
-
-        #action = random.choice(Environment.valid_actions)
-        action = self.next_waypoint
-
+        # choose next action
+        # next_action = random.choice(self.actions)
+        max_action_index = self.next_action_index(current_state)
         # Execute action and get reward
-        reward += self.env.act(self, action)
+        reward = self.env.act(self, self.actions[max_action_index])
 
         # TODO: Learn policy based on state, action, reward
+        # Q(state, action) = Q(state, action) + alpha * (R(state, action) + Gamma * Max[Q(next state, all actions)])
+        if (t != 0):
+            
 
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
     def get_state(self):
         return 0
@@ -59,7 +63,7 @@ def run():
     a = e.create_agent(LearningAgent)  # create agent
     e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
-
+    current_state = 
     # Now simulate it
     sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
